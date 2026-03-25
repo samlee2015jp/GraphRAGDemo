359097477386a51371d3046fa625da1244bc78e6
Sam Li <samlee2015jp@gmail.com>
2026-03-25 20:07:34 +0900

    add

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import scripts.compliance_ingestor as compliance_ingestor
 
 
 # 初始化环境并扫描文件
-compliance_ingestor.setup_compliance_environment()
-compliance_ingestor.scan_compliance_files()
+# compliance_ingestor.setup_compliance_environment()
+# compliance_ingestor.scan_compliance_files()
